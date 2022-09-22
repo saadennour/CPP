8e650dda734930505b824238e8b70c0efe657366
saad-ennour <saadennourfarhan@gmail.com>
2022-09-22 15:01:22 +0100

    ClapTrap

---
+++ C03/ex00/main.cpp
@@ -0,0 +1,23 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+    ClapTrap    aemon("aemon");
+    ClapTrap    vis(aemon);
+    ClapTrap    ana("eagon");
+
+    vis = ana;
+    vis.attack("aemon");
+    vis.attack("aemon");
+    vis.attack("aemon");
+    vis.attack("aemon");
+    vis.attack("aemon");
+    vis.attack("aemon");
+    vis.attack("aemon");
+    vis.attack("aemon");
+    vis.attack("aemon");
+    vis.attack("aemon");
+    vis.attack("aemon");
+    vis.attack("aemon");
+    return (0);
+}
